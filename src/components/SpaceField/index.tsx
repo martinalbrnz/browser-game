a8f23ccfc91ab9abcdac7bd2f41bafcba65c86f8
martinalbrnz <martinalbor96@gmail.com>
2023-01-25 00:00:13 -0300

    Add single player collision checker

--- src/components/SpaceField/index.tsx
+++ src/components/SpaceField/index.tsx
@@ -1,13 +1,14 @@
-import { createEffect, createSignal, onCleanup } from "solid-js";
+import { createSignal, onCleanup } from "solid-js";
 import styles from "./space-field.module.scss";
 
 const SpaceField = () => {
   const [ball, setBall] = createSignal({ x: 200, y: 80 });
   const [mov, setMov] = createSignal({ x: 1, y: 1 });
 
-  const [player, setPlayer] = createSignal({ x: 400, y: 280 });
+  const [player, setPlayer] = createSignal({ x: 240, y: 120 });
 
   const ballSize = 10;
+  const playerSize = 20;
   const maxWidth = 480;
   const maxHeight = 360;
 
@@ -20,10 +21,19 @@ const SpaceField = () => {
     if (ball().y <= 0) setMov({ y: Math.abs(mov().y), x: mov().x });
   }
 
+  function normalize(): void {}
+
+  function checkPlayerCollision(): boolean {
+    const collX = Math.abs(ball().x - player().x) < (ballSize + playerSize) / 2;
+    const collY = Math.abs(ball().y - player().y) < (ballSize + playerSize) / 2;
+    return collX && collY;
+  }
+
   const tick = setInterval(() => {
     checkCollision();
+    // checkPlayerCollision();
     setBall({ x: ball().x + mov().x, y: ball().y + mov().y });
-  }, 8);
+  }, 100);
 
   onCleanup(() => clearInterval(tick));
 
@@ -32,15 +42,17 @@ const SpaceField = () => {
       <div class={styles.positionContainer}>
         <p>X: {ball().x}</p>
         <p>Y: {ball().y}</p>
+        <p>Collision?: {() => (checkPlayerCollision() ? "true" : "false")}</p>
       </div>
       <div class={styles.field}>
         <div
           class={styles.ball}
           style={{ top: `${ball().y}px`, left: `${ball().x}px` }}
         ></div>
-				<div class={styles.player}
-					style={{top: `${player().y}px`, left: `${player().x}px`}}
-				></div>
+        <div
+          class={styles.player}
+          style={{ top: `${player().y}px`, left: `${player().x}px` }}
+        ></div>
       </div>
     </div>
   );
