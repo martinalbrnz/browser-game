ddb9247400a8945082df88452606d5d38ec4aae5
martinalbrnz <martinalbor96@gmail.com>
2023-01-25 02:37:32 -0300

    Add a poor movement implementation
    - Working so far
    - A bit buggy

--- src/components/SpaceField/index.tsx
+++ src/components/SpaceField/index.tsx
@@ -1,9 +1,10 @@
+import { fromEvent, throttleTime } from "rxjs";
 import { createSignal, onCleanup } from "solid-js";
 import styles from "./space-field.module.scss";
 
 const SpaceField = () => {
-  const [ball, setBall] = createSignal({ x: 245, y: 125 });
-  const [mov, setMov] = createSignal({ x: 1, y: 1 });
+  const [ball, setBall] = createSignal({ x: 10, y: 136 });
+  const [mov, setMov] = createSignal({ x: 1, y: 0 });
 
   const [player, setPlayer] = createSignal({ x: 265, y: 145 });
 
@@ -11,6 +12,14 @@ const SpaceField = () => {
   const playerSize = 20;
   const maxWidth = 480;
   const maxHeight = 360;
+  const moves$ = fromEvent(document, "keydown")
+    .pipe(throttleTime(100))
+    .subscribe((val: any) => {
+      if (val.key === "a") setPlayer({ x: player().x - 10, y: player().y });
+      if (val.key === "w") setPlayer({ x: player().x, y: player().y - 10 });
+      if (val.key === "d") setPlayer({ x: player().x + 10, y: player().y });
+      if (val.key === "s") setPlayer({ x: player().x, y: player().y + 10 });
+    });
 
   function checkCollision(): void {
     if (ball().x >= maxWidth - ballSize)
@@ -21,7 +30,11 @@ const SpaceField = () => {
     if (ball().y <= 0) setMov({ y: Math.abs(mov().y), x: mov().x });
   }
 
-  function normalize(angle: number, magnitude: number = 1): void {}
+  function normalize(angle: number, magnitude: number = 1): void {
+    const i = Math.cos(angle) * magnitude;
+    const j = Math.sin(angle) * magnitude;
+    setMov({ x: i, y: j });
+  }
 
   function checkPlayerCollision(): boolean {
     return (
@@ -36,33 +49,34 @@ const SpaceField = () => {
   }
 
   function bounce() {
-    const ballAngle =
-      (Math.atan2(
-        ball().y + ballSize / 2 - (player().y + playerSize / 2),
-        ball().x + ballSize / 2 - (player().x + playerSize / 2)
-      ) /
-        Math.PI) *
-      180;
-    const movAngle = (Math.atan2(mov().y, mov().x) / Math.PI) * 180;
+    const ballAngle = Math.atan2(
+      ball().y + ballSize / 2 - (player().y + playerSize / 2),
+      ball().x + ballSize / 2 - (player().x + playerSize / 2)
+    );
+    const movAngle = Math.atan2(mov().y, mov().x);
     const newAngle = ballAngle - movAngle;
+    normalize(-newAngle);
   }
 
   const tick = setInterval(() => {
     checkCollision();
     if (checkPlayerCollision()) bounce();
     setBall({ x: ball().x + mov().x, y: ball().y + mov().y });
-  }, 16);
+  }, 5);
 
   const stop = () => clearInterval(tick);
-
-  onCleanup(() => clearInterval(tick));
+  onCleanup(() => {
+    clearInterval(tick);
+    moves$.unsubscribe();
+  });
 
   return (
     <div class={styles.fieldContainer}>
       <div class={styles.positionContainer}>
-        <p>X: {ball().x}</p>
-        <p>Y: {ball().y}</p>
-        <button onclick={stop}>Stop</button>
+        <p>X: {ball().x.toFixed()}</p>
+        <p>Y: {ball().y.toFixed()}</p>
+        <p>Mx: {mov().x.toFixed(2)}</p>
+        <p>My: {mov().y.toFixed(2)}</p>
       </div>
       <div class={styles.field}>
         <div
