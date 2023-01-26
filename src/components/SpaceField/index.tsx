36c103c9ba2f8f4f97e41180bb014bd7f4d8b3aa
martinalbrnz <martinalbor96@gmail.com>
2023-01-25 22:31:12 -0300

    Simplified bounce
    - Simplified bounce to 4 directions
    - Add more players
    - Made players and ball square

--- src/components/SpaceField/index.tsx
+++ src/components/SpaceField/index.tsx
@@ -1,27 +1,51 @@
-import { fromEvent, throttleTime } from "rxjs";
+import { interval } from "rxjs";
 import { createSignal, onCleanup } from "solid-js";
 import styles from "./space-field.module.scss";
 
 const SpaceField = () => {
-  const [ball, setBall] = createSignal({ x: 10, y: 136 });
-  const [mov, setMov] = createSignal({ x: 1, y: 0 });
+  const [ball, setBall] = createSignal({ x: 190, y: 191 });
+  const [mov, setMov] = createSignal({ x: 1, y: 1 });
 
-  const [player, setPlayer] = createSignal({ x: 265, y: 145 });
+  const [playersA, setPlayersA] = createSignal([
+    { id: 1, x: 120, y: 200 },
+    { id: 2, x: 100, y: 160 },
+    { id: 3, x: 140, y: 280 },
+    { id: 4, x: 210, y: 211 },
+    { id: 5, x: 210, y: 200 },
+    { id: 6, x: 90, y: 235 },
+    { id: 7, x: 90, y: 235 },
+    { id: 8, x: 90, y: 235 },
+    { id: 9, x: 90, y: 235 },
+    { id: 10, x: 90, y: 235 },
+    { id: 11, x: 90, y: 235 },
+  ]);
+  const [playersB, setPlayersB] = createSignal([
+    { id: 1, x: 165, y: 160 },
+    { id: 2, x: 165, y: 250 },
+    { id: 3, x: 200, y: 280 },
+    { id: 4, x: 200, y: 170 },
+    { id: 5, x: 120, y: 255 },
+    { id: 6, x: 180, y: 140 },
+    { id: 7, x: 180, y: 140 },
+    { id: 8, x: 180, y: 140 },
+    { id: 9, x: 180, y: 140 },
+    { id: 10, x: 180, y: 140 },
+    { id: 11, x: 180, y: 140 },
+  ]);
 
   const ballSize = 10;
   const playerSize = 20;
   const maxWidth = 480;
   const maxHeight = 360;
-  const moves$ = fromEvent(document, "keydown")
-    .pipe(throttleTime(100))
-    .subscribe((val: any) => {
-      if (val.key === "a") setPlayer({ x: player().x - 10, y: player().y });
-      if (val.key === "w") setPlayer({ x: player().x, y: player().y - 10 });
-      if (val.key === "d") setPlayer({ x: player().x + 10, y: player().y });
-      if (val.key === "s") setPlayer({ x: player().x, y: player().y + 10 });
-    });
+  const tickTime = 100;
 
-  function checkCollision(): void {
+  const tick$ = interval(tickTime).subscribe(() => {
+    moveBall();
+    checkWallCollision();
+    checkPlayersCollision();
+  });
+
+  function checkWallCollision(): void {
     if (ball().x >= maxWidth - ballSize)
       setMov({ x: -Math.abs(mov().x), y: mov().y });
     if (ball().x <= 0) setMov({ x: Math.abs(mov().x), y: mov().y });
@@ -30,44 +54,75 @@ const SpaceField = () => {
     if (ball().y <= 0) setMov({ y: Math.abs(mov().y), x: mov().x });
   }
 
-  function normalize(angle: number, magnitude: number = 1): void {
-    const i = Math.cos(angle) * magnitude;
-    const j = Math.sin(angle) * magnitude;
-    setMov({ x: i, y: j });
-  }
-
-  function checkPlayerCollision(): boolean {
-    return (
-      Math.abs(
-        Math.sqrt(
-          Math.pow(ball().x + ballSize / 2 - (player().x + playerSize / 2), 2) +
-            Math.pow(ball().y + ballSize / 2 - (player().y + playerSize / 2), 2)
-        )
-      ) <=
-      (ballSize + playerSize) / 2
-    );
-  }
-
-  function bounce() {
-    const ballAngle = Math.atan2(
-      ball().y + ballSize / 2 - (player().y + playerSize / 2),
-      ball().x + ballSize / 2 - (player().x + playerSize / 2)
-    );
-    const movAngle = Math.atan2(mov().y, mov().x);
-    const newAngle = ballAngle - movAngle;
-    normalize(-newAngle);
-  }
-
-  const tick = setInterval(() => {
-    checkCollision();
-    if (checkPlayerCollision()) bounce();
+  function moveBall() {
     setBall({ x: ball().x + mov().x, y: ball().y + mov().y });
-  }, 5);
+  }
 
-  const stop = () => clearInterval(tick);
+  function bounce(dir: string) {
+    switch (dir) {
+      case "right":
+        setMov({ x: 1, y: mov().y });
+        break;
+      case "left":
+        setMov({ x: -1, y: mov().y });
+        break;
+      case "up":
+        setMov({ x: mov().x, y: -1 });
+        break;
+      case "down":
+        setMov({ x: mov().x, y: 1 });
+        break;
+    }
+  }
+
+  function checkItemCollision(posX: number, posY: number) {
+    const diffX = posX - ball().x;
+    const diffY = posY - ball().y;
+    if (
+      diffX > 0 &&
+      diffX <= ballSize &&
+      diffY < ballSize &&
+      diffY > -playerSize
+    ) {
+      bounce("left");
+    }
+    if (
+      diffX < 0 &&
+      diffX >= -playerSize &&
+      diffY < ballSize &&
+      diffY > -playerSize
+    ) {
+      bounce("right");
+    }
+    if (
+      diffY > 0 &&
+      diffY <= ballSize &&
+      diffX < ballSize &&
+      diffX > -playerSize
+    ) {
+      bounce("up");
+    }
+    if (
+      diffY < 0 &&
+      diffY >= -playerSize &&
+      diffX <= ballSize &&
+      diffX > -playerSize
+    ) {
+      bounce("down");
+    }
+  }
+
+  function checkPlayersCollision() {
+    playersA().map((player) => {
+      checkItemCollision(player.x, player.y);
+    });
+    playersB().map((player) => {
+      checkItemCollision(player.x, player.y);
+    });
+  }
+
   onCleanup(() => {
-    clearInterval(tick);
-    moves$.unsubscribe();
+    tick$.unsubscribe();
   });
 
   return (
@@ -75,18 +130,30 @@ const SpaceField = () => {
       <div class={styles.positionContainer}>
         <p>X: {ball().x.toFixed()}</p>
         <p>Y: {ball().y.toFixed()}</p>
-        <p>Mx: {mov().x.toFixed(2)}</p>
-        <p>My: {mov().y.toFixed(2)}</p>
+        {/* <p>Mx: {mov().x.toFixed(2)}</p>
+        <p>My: {mov().y.toFixed(2)}</p> */}
       </div>
       <div class={styles.field}>
         <div
           class={styles.ball}
           style={{ top: `${ball().y}px`, left: `${ball().x}px` }}
         ></div>
-        <div
-          class={styles.player}
-          style={{ top: `${player().y}px`, left: `${player().x}px` }}
-        ></div>
+        {playersA().map((player) => {
+          return (
+            <div
+              class={`${styles.player} ${styles.playerA}`}
+              style={{ top: `${player.y}px`, left: `${player.x}px` }}
+            ></div>
+          );
+        })}
+        {playersB().map((player) => {
+          return (
+            <div
+              class={`${styles.player} ${styles.playerB}`}
+              style={{ top: `${player.y}px`, left: `${player.x}px` }}
+            ></div>
+          );
+        })}
       </div>
     </div>
   );
