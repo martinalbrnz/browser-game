72c8fb94e336e168b26e6da37bdb73ea0880b9cf
martinalbrnz <martinalbor96@gmail.com>
2023-01-26 21:35:11 -0300

    Add score and goal counter

--- src/components/SpaceField/index.tsx
+++ src/components/SpaceField/index.tsx
@@ -3,41 +3,48 @@ import { createSignal, onCleanup } from "solid-js";
 import styles from "./space-field.module.scss";
 
 const SpaceField = () => {
-  const [ball, setBall] = createSignal({ x: 190, y: 191 });
+  const [ball, setBall] = createSignal({ x: 190, y: 340 });
   const [mov, setMov] = createSignal({ x: 1, y: 1 });
+  const [score, setScore] = createSignal({ a: 0, b: 0 });
 
   const [playersA, setPlayersA] = createSignal([
-    { id: 1, x: 120, y: 200 },
-    { id: 2, x: 100, y: 160 },
-    { id: 3, x: 140, y: 280 },
-    { id: 4, x: 210, y: 211 },
-    { id: 5, x: 210, y: 200 },
-    { id: 6, x: 90, y: 235 },
-    { id: 7, x: 90, y: 235 },
-    { id: 8, x: 90, y: 235 },
-    { id: 9, x: 90, y: 235 },
-    { id: 10, x: 90, y: 235 },
-    { id: 11, x: 90, y: 235 },
+    { id: 1, x: 10, y: 170 },
+    { id: 2, x: 70, y: 70 },
+    { id: 3, x: 70, y: 270 },
+    { id: 4, x: 70, y: 170 },
+    { id: 5, x: 198, y: 290 },
+    { id: 6, x: 198, y: 200 },
+    { id: 7, x: 198, y: 140 },
+    { id: 8, x: 198, y: 50 },
+    { id: 9, x: 326, y: 70 },
+    { id: 10, x: 326, y: 170 },
+    { id: 11, x: 326, y: 270 },
   ]);
   const [playersB, setPlayersB] = createSignal([
-    { id: 1, x: 165, y: 160 },
-    { id: 2, x: 165, y: 250 },
-    { id: 3, x: 200, y: 280 },
-    { id: 4, x: 200, y: 170 },
-    { id: 5, x: 120, y: 255 },
-    { id: 6, x: 180, y: 140 },
-    { id: 7, x: 180, y: 140 },
-    { id: 8, x: 180, y: 140 },
-    { id: 9, x: 180, y: 140 },
-    { id: 10, x: 180, y: 140 },
-    { id: 11, x: 180, y: 140 },
+    { id: 1, x: 450, y: 170 },
+    { id: 2, x: 390, y: 70 },
+    { id: 3, x: 390, y: 270 },
+    { id: 4, x: 390, y: 170 },
+    { id: 5, x: 262, y: 290 },
+    { id: 6, x: 262, y: 200 },
+    { id: 7, x: 262, y: 140 },
+    { id: 8, x: 262, y: 50 },
+    { id: 9, x: 134, y: 70 },
+    { id: 10, x: 134, y: 170 },
+    { id: 11, x: 134, y: 270 },
   ]);
 
   const ballSize = 10;
   const playerSize = 20;
   const maxWidth = 480;
   const maxHeight = 360;
-  const tickTime = 100;
+  const goalHeight = 330;
+  const goalWidth = 10;
+  const tickTime = 13;
+  const ballInitial = {
+    x: (maxWidth - ballSize) / 2,
+    y: (maxHeight - ballSize) / 2,
+  };
 
   const tick$ = interval(tickTime).subscribe(() => {
     moveBall();
@@ -46,9 +53,26 @@ const SpaceField = () => {
   });
 
   function checkWallCollision(): void {
-    if (ball().x >= maxWidth - ballSize)
+    if (ball().x >= maxWidth - ballSize) {
       setMov({ x: -Math.abs(mov().x), y: mov().y });
-    if (ball().x <= 0) setMov({ x: Math.abs(mov().x), y: mov().y });
+      const insideGoal =
+        ball().y + ballSize / 2 >= (maxHeight - goalHeight) / 2 &&
+        ball().y + ballSize / 2 <= (maxHeight - goalHeight) / 2 + goalHeight;
+      if (insideGoal) {
+        setScore({ a: score().a + 1, b: score().b });
+        setBall(ballInitial);
+      }
+    }
+    if (ball().x <= 0) {
+      setMov({ x: Math.abs(mov().x), y: mov().y });
+      const insideGoal =
+        ball().y + ballSize / 2 >= (maxHeight - goalHeight) / 2 &&
+        ball().y + ballSize / 2 <= (maxHeight - goalHeight) / 2 + goalHeight;
+      if (insideGoal) {
+        setScore({ a: score().a, b: score().b + 1 });
+        setBall(ballInitial);
+      }
+    }
     if (ball().y >= maxHeight - ballSize)
       setMov({ y: -Math.abs(mov().y), x: mov().x });
     if (ball().y <= 0) setMov({ y: Math.abs(mov().y), x: mov().x });
@@ -127,13 +151,29 @@ const SpaceField = () => {
 
   return (
     <div class={styles.fieldContainer}>
-      <div class={styles.positionContainer}>
-        <p>X: {ball().x.toFixed()}</p>
-        <p>Y: {ball().y.toFixed()}</p>
-        {/* <p>Mx: {mov().x.toFixed(2)}</p>
-        <p>My: {mov().y.toFixed(2)}</p> */}
-      </div>
+      <h2>
+        {score().a} : {score().b}
+      </h2>
       <div class={styles.field}>
+        <div
+          class={styles.goal}
+          style={{
+            top: `${(maxHeight - goalHeight) / 2}px`,
+            left: `${-goalWidth}px`,
+            height: `${goalHeight}px`,
+            width: `${goalWidth}px`,
+          }}
+        ></div>
+        <div
+          class={styles.goal}
+          style={{
+            top: `${(maxHeight - goalHeight) / 2}px`,
+            left: `${maxWidth}px`,
+            height: `${goalHeight}px`,
+            width: `${goalWidth}px`,
+          }}
+        ></div>
+
         <div
           class={styles.ball}
           style={{ top: `${ball().y}px`, left: `${ball().x}px` }}
@@ -143,7 +183,9 @@ const SpaceField = () => {
             <div
               class={`${styles.player} ${styles.playerA}`}
               style={{ top: `${player.y}px`, left: `${player.x}px` }}
-            ></div>
+            >
+              <span>{player.id}</span>
+            </div>
           );
         })}
         {playersB().map((player) => {
@@ -151,9 +193,15 @@ const SpaceField = () => {
             <div
               class={`${styles.player} ${styles.playerB}`}
               style={{ top: `${player.y}px`, left: `${player.x}px` }}
-            ></div>
+            >
+              <span>{player.id}</span>
+            </div>
           );
         })}
+      </div>
+      <div class={styles.positionContainer}>
+        <p>X: {ball().x.toFixed()}</p>
+        <p>Y: {ball().y.toFixed()}</p>
       </div>
     </div>
   );
