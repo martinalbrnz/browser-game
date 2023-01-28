98e602f1c1334d488606a20db502dec6e05bea50
martinalbrnz <martinalbor96@gmail.com>
2023-01-28 19:34:13 -0300

    Add basic comps to game

--- src/components/TenisField/index.tsx
+++ src/components/TenisField/index.tsx
@@ -1,9 +1,44 @@
+import styles from "./tenis-field.module.scss";
+
 import { createSignal } from "solid-js";
 
 const TenisField = () => {
-  const [ball, setBall] = createSignal({ x: 0, y: 0 });
+  const ballSize = 10
+  const playerSize = 20
+
+  const [ball, setBall] = createSignal({ x: 200, y: 400 });
   const [ballMov, setBallMov] = createSignal({ x: 0, y: 0 });
-  return <div>Tenis field</div>;
+  const [playerA, setPlayerA] = createSignal(100);
+  const [playerB, setPlayerB] = createSignal(400);
+  return <div class={styles.field}>
+    <div
+      class={styles.ball}
+      style={{
+        width: `${ballSize}px`,
+        height: `${ballSize}px`,
+        top: `${ball().y}px`,
+        left: `${ball().x}px`
+      }}
+    ></div>
+    <div
+      class={styles.playerA}
+      style={{
+        width: `${playerSize}px`,
+        height: `${playerSize}px`,
+        top: `400px`,
+        left: `${playerA()}px`
+      }}
+    ></div>
+    <div
+      class={styles.playerB}
+      style={{
+        width: `${playerSize}px`,
+        height: `${playerSize}px`,
+        top: `400px`,
+        left: `${playerB()}px`
+      }}
+    ></div>
+  </div>;
 };
 
-export default TenisField;
+export default TenisField
